a0d5728758b9fa15e83cb9721ee74d1523f65247
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-18 19:32:46 -0700

    Add the Cart component

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './style.css';
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            <div className="close">[close]</div>
+            <h2>Shopping Cart</h2>
+            <div>
+                <CartItem item={{ name: 'Camera', image: 'camera.jpg', price: 5, purchaseQuantity: 3 }} />
+                <CartItem item={{ name: 'Soap', image: 'soap.jpg', price: 6, purchaseQuantity: 4 }} />
+
+                <div className="flex-row space-betwee">
+                    <strong>Total: $0</strong>
+                    {Auth.loggedIn ? <button>Checkout</button> : <span>(login to checkout)</span>}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
